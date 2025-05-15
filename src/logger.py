ec4757e0456c7f0ec9453ea946db34fb2cb104a9
heitorpcrl <128477989+heitorpcrl@users.noreply.github.com>
2025-05-15 20:13:57 -0300

    logger.py
    feat: Minor changes

--- src/logger.py
+++ src/logger.py
@@ -3,11 +3,24 @@ from datetime import datetime
 
 class Colors:
     
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    
     BRIGHT_RED = '\033[91m'
     BRIGHT_GREEN = '\033[92m'
     BRIGHT_YELLOW = '\033[93m'
     BRIGHT_BLUE = '\033[94m'
     BRIGHT_MAGENTA = '\033[95m'
+    BRIGHT_CYAN = '\033[96m'
 
 class ColoredFormatter(logging.Formatter):
     
